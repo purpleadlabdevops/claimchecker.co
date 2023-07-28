beea45091231dd82398a923c5e3a02154c4abf28
Max Globa <onyx18121990@gmail.com>
2023-07-28 19:42:20 +0300

    added form

---
+++ api/index.js
@@ -0,0 +1,62 @@
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  request = require('request'),
+  nodemailer = require('nodemailer'),
+  app = express(),
+  db = require('./db')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/email', (req, res) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SG_HOST,
+    port: process.env.SG_PORT,
+    secure: false,
+    auth: {
+      user: 'apikey',
+      pass: process.env.SG_KEY,
+    }
+  })
+  transporter.sendMail({
+    from: '"Financial Match" <support@geekex.com>',
+    to: `referrals@financialmatch.com`,
+    subject: `Referral Partner Program | Financial Match`,
+    html: `
+      <p>First name: <strong>${req.body.params.firstName}</strong></p>
+      <p>Last name: <strong>${req.body.params.lastName}</strong></p>
+      <p>Email: <strong>${req.body.params.email}</strong></p>
+      <p>Phone: <strong>${req.body.params.phone}</strong></p>
+      <p>Notes: <strong>${req.body.params.notes}</strong></p>
+    `
+  })
+    .then(response => {
+      res.send({
+        status: 'success',
+        msg: response
+      })
+    })
+    .catch(error => {
+      res.send({
+        status: 'error',
+        msg: error
+      })
+    })
+})
+
+app.route("/db")
+  .get((req, res) => {
+    db(`SELECT * FROM partners`)
+      .then(rows => res.send(rows))
+      .catch(err => res.send({status: 'error', msg: err}))
+  })
+  .post((req, res) => {
+    db(`INSERT INTO partners (firstName, lastName, email, phone, notes) VALUES ('${req.body.params.firstName}', '${req.body.params.lastName}', '${req.body.params.email}', '${req.body.params.phone}', '${req.body.params.notes}')`)
+      .then(rows => res.send(rows))
+      .catch(err => res.send({status: 'error', msg: err}))
+  })
+
+module.exports = {
+  path: '/api',
+  handler: app,
+}
---
+++ pages/partners.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="partners">
+    <div class="container">
+      <div class="partners__list" v-if="list">
+        <div class="partners__item" v-for="item in list.slice().reverse()">
+          <h3>{{ item.firstName }} {{ item.lastName }}</h3>
+          <p>email: <span>{{ item.email }}</span></p>
+          <p>phone: <span>{{ item.phone }}</span></p>
+          <p>notes: <span>{{ item.notes }}</span></p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      list: null,
+    }
+  },
+  mounted(){
+    this.$axios.get(`${process.env.API}/db`, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(dbResult => {
+        this.list = dbResult.data
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.partners{
+  padding-top: 60px;
+  padding-bottom: 60px;
+  &__item{
+    max-width: 400px;
+    background: var(--bg-white);
+    box-shadow: 0 0 16px var(--bg-gray);
+    border-radius: 12px;
+    width: 100%;
+    margin: 0 auto;
+    position: relative;
+    padding: 20px 15px;
+    margin-bottom: 30px;
+  }
+  h3{
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 1.3;
+    color: var(--text-black);
+    margin-bottom: 10px;
+  }
+  p{
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 1.4;
+    color: var(--text-black);
+    margin-bottom: 5px;
+    span{
+      font-weight: 600;
+      font-size: 16px;
+    }
+  }
+}
+</style>
