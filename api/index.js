69517e2d9835697265a9df7ccd5fb8ce60721711
Max Globa <onyx18121990@gmail.com>
2023-08-02 14:55:39 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -32,6 +32,7 @@ app.route("/db")
     db(`INSERT INTO users (card, type, fullName, phone, email, company, address, ein) VALUES ('${req.body.params.card}', '${req.body.params.type}', '${req.body.params.fullName}', '${req.body.params.phone}', '${req.body.params.email}', '${req.body.params.company}', '${req.body.params.address}', '${req.body.params.ein}')`)
       .then(rows => {
         fileDOCX(req.body.params.fullName, req.body.params.company, rows.insertId)
+        console.log(rows.insertId);
         return rows.insertId
       })
       .then(ID => {
