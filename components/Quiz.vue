4e4344061f57adb966efb326b6f4d7a66e1d17d5
Max Globa <onyx18121990@gmail.com>
2023-08-14 13:46:06 +0300

    Update Quiz.vue

--- components/Quiz.vue
+++ components/Quiz.vue
@@ -277,7 +277,7 @@ export default {
           console.dir(result);
           setTimeout(()=>{
             this.signNowInvite(result.data.msg.access_token, result.data.msg.pdfID, result.data.msg.docxID)
-          }, 5000);
+          }, 10000);
         })
         .catch(err => {
           console.dir(err);
